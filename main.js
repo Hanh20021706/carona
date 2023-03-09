935cdaa5a1c33ae4cb8d8a1efde590df57c3d16c
Hanh20021706 <hanhs8155@gmail.com>
2023-03-09 10:02:16 +0700

    init project : section page

--- main.js
+++ main.js
@@ -21,17 +21,50 @@ function showDivs(n) {
 }
 
 //   tabs
-function openCity(evt, cityName) {
-    console.log('fjhdsfgh');
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+// function openselect(evt, btnName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tabslink");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace("active", "");
+//   }
+//   document.getElementById(btnName).style.display = "block";
+//   evt.currentTarget.className += "active";
+// }
+
+
+// tabs detail
+function openselect(evt, contentName) {
+  var i, tablinks;
+  var x = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tabslink");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" activeTab", "");
   }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(contentName).style.display = "block";
+  evt.currentTarget.className += " activeTab";
+}
+
+// tab sliec customer 
+function openSlice(evt, sliceName) {
+  console.log('saiii');
+  var i, sliceLink;
+  var x = document.getElementsByClassName("sliceContent");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  sliceLink = document.getElementsByClassName("btnTabs");
+  for (i = 0; i < sliceLink.length; i++) {
+    sliceLink[i].className = sliceLink[i].className.replace(" activeslicce", "");
+  }
+  document.getElementById(sliceName).style.display = "flex";
+  evt.currentTarget.className += " activeslicce";
+
+  document.getElementById("defaultOpen").click();
 }
